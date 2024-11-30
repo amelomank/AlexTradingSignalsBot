4a55fa0f0735c12e31f41936e780d9a98a351daf
amelomank <kotkotovskij437@gmail.com>
2024-12-01 01:17:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -640,7 +640,7 @@ async def обработчик_кнопок_пользователя(message: ty
 async def тики_бота():
     тики = 0
     while True:
-        await asyncio.sleep(60)
+        await asyncio.sleep(100)
         тики += 1
         print(тики)
 print('Бот запущен!')
